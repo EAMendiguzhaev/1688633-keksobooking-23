5677acd8dadeef4427cc536514f8b13a5b1db449
BananaRepublic14 <mendiguzhaevea@gmail.com>
2021-05-29 13:57:30 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Функция возвращающая случайное целое число из переданного диапазона включительно.
+
+const randomInteger = function (min, max) {
+  // случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+randomInteger(0, 100);
+
+/*
+
+Либо вот такой вариант:
+const randomInteger = function (min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+randomInteger(0, 100);
+
+*/
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+
+const getFractionalNumber = function (min, max, fixed) {
+  const randomNumber = Math.random() * (max + 1 - min) + min;
+  return randomNumber.toFixed(fixed);
+};
+
+getFractionalNumber(0, 100.777, 2);
