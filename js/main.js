ec3a12e40438d6276d442aa3417f9716f70b704c
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-06 16:28:57 +0300

    Улучшаю читаемость кода

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assetshtmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
-const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const checkins = ['12:00', '13:00', '14:00'];
-const checkouts = ['12:00', '13:00', '14:00'];
-
-const mocksConfig = {
+const MocksConfig = {
+  OFFERS_COUNT: 14,
   AVATAR: {
     MIN: 1,
     MAX: 8,
@@ -17,6 +8,7 @@ const mocksConfig = {
     MIN: 1000,
     MAX: 10000,
   },
+  TYPE: ['palace', 'flat', 'house', 'bungalow', 'hotel'],
   ROOMS: {
     MIN: 1,
     MAX: 7,
@@ -25,6 +17,14 @@ const mocksConfig = {
     MIN: 1,
     MAX: 14,
   },
+  CHECKIN: ['12:00', '13:00', '14:00'],
+  CHECKOUT: ['12:00', '13:00', '14:00'],
+  FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  PHOTOS: [
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assetshtmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  ],
   LAT: {
     MIN: 35.65,
     MAX: 35.7,
@@ -58,24 +58,24 @@ const getRandomItems = (items, count) => {
   return shuffledItems.slice(0, count);
 };
 
-const getRandomLat = getRandomPositiveFloat(mocksConfig.LAT.MIN, mocksConfig.LAT.MAX, 5);
-const getRandomLng = getRandomPositiveFloat(mocksConfig.LNG.MIN, mocksConfig.LNG.MAX, 5);
+const getRandomLat = getRandomPositiveFloat(MocksConfig.LAT.MIN, MocksConfig.LAT.MAX, 5);
+const getRandomLng = getRandomPositiveFloat(MocksConfig.LNG.MIN, MocksConfig.LNG.MAX, 5);
 
 // Создает новый объект с рандомными свойствами
 const createOffer = () => ({
-  avatar: `img/avatars/user0${getRandomPositiveInteger(mocksConfig.AVATAR.MIN, mocksConfig.AVATAR.MAX)}.png`,
+  avatar: `img/avatars/user0${getRandomPositiveInteger(MocksConfig.AVATAR.MIN, MocksConfig.AVATAR.MAX)}.png`,
   offer: {
     title: 'Добро пожаловать',
     address: `${getRandomLat}, ${getRandomLng}`,
-    price: getRandomPositiveInteger(mocksConfig.PRICE.MIN, mocksConfig.PRICE.MAX),
-    type: getRandomItems(types, getRandomPositiveInteger(1, 1)),
-    rooms: getRandomPositiveInteger(mocksConfig.ROOMS.MIN, mocksConfig.ROOMS.MAX),
-    guests: getRandomPositiveInteger(mocksConfig.QUESTS.MIN, mocksConfig.QUESTS.MAX),
-    checkin: getRandomItems(checkins, getRandomPositiveInteger(1, 1)),
-    checkout: getRandomItems(checkouts, getRandomPositiveInteger(1, 1)),
-    features: getRandomItems(features, getRandomPositiveInteger(0, features.length)),
+    price: getRandomPositiveInteger(MocksConfig.PRICE.MIN, MocksConfig.PRICE.MAX),
+    type: getRandomItems(MocksConfig.TYPE, getRandomPositiveInteger(1, 1)),
+    rooms: getRandomPositiveInteger(MocksConfig.ROOMS.MIN, MocksConfig.ROOMS.MAX),
+    guests: getRandomPositiveInteger(MocksConfig.QUESTS.MIN, MocksConfig.QUESTS.MAX),
+    checkin: getRandomItems(MocksConfig.CHECKIN, getRandomPositiveInteger(1, 1)),
+    checkout: getRandomItems(MocksConfig.CHECKOUT, getRandomPositiveInteger(1, 1)),
+    features: getRandomItems(MocksConfig.FEATURES, getRandomPositiveInteger(0, MocksConfig.FEATURES.length)),
     description: 'Комфортабельное и уютное помещение',
-    photos: getRandomItems(photos, getRandomPositiveInteger(0, features.length)),
+    photos: getRandomItems(MocksConfig.PHOTOS, getRandomPositiveInteger(1, MocksConfig.PHOTOS.length)),
   },
   location: {
     lat: getRandomLat,
@@ -85,4 +85,4 @@ const createOffer = () => ({
 
 // Создает новые рандомные объекты
 const createOffers = (count) => new Array(count).fill(null).map(() => createOffer());
-createOffers(14);
+createOffers(MocksConfig.OFFERS_COUNT);
