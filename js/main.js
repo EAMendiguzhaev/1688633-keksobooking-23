4388514eea19386b886af829d280b9181e07f32d
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-06 16:11:00 +0300

    Исправляю ДЗ после проверки наставником

--- js/main.js
+++ js/main.js
@@ -4,6 +4,36 @@ const photos = [
   'https://assetshtmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
+const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkins = ['12:00', '13:00', '14:00'];
+const checkouts = ['12:00', '13:00', '14:00'];
+
+const mocksConfig = {
+  AVATAR: {
+    MIN: 1,
+    MAX: 8,
+  },
+  PRICE: {
+    MIN: 1000,
+    MAX: 10000,
+  },
+  ROOMS: {
+    MIN: 1,
+    MAX: 7,
+  },
+  QUESTS: {
+    MIN: 1,
+    MAX: 14,
+  },
+  LAT: {
+    MIN: 35.65,
+    MAX: 35.7,
+  },
+  LNG: {
+    MIN: 35.65,
+    MAX: 35.7,
+  },
+};
 
 const getRandomPositiveInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -28,33 +58,31 @@ const getRandomItems = (items, count) => {
   return shuffledItems.slice(0, count);
 };
 
-const getRandomLat = getRandomPositiveFloat(35.65, 35.7, 5);
-const getRandomLng = getRandomPositiveFloat(35.65, 35.7, 5);
+const getRandomLat = getRandomPositiveFloat(mocksConfig.LAT.MIN, mocksConfig.LAT.MAX, 5);
+const getRandomLng = getRandomPositiveFloat(mocksConfig.LNG.MIN, mocksConfig.LNG.MAX, 5);
 
 // Создает новый объект с рандомными свойствами
-const createAuthor = function () {
-  return {
-    avatar: `img/avatars/user0${getRandomPositiveInteger(1, 8)}.png`,
-    offer: {
-      title: 'Добро пожаловать',
-      address: `${getRandomLat}, ${getRandomLng}`,
-      price: getRandomPositiveInteger(1000, 10000),
-      type: 'house',
-      rooms: getRandomPositiveInteger(1, 7),
-      guests: getRandomPositiveInteger(1, 14),
-      checkout: '14:00',
-      features: getRandomItems(features, getRandomPositiveInteger(0, features.length)),
-      description: 'Комфортабельное и уютное помещение',
-      photos: getRandomItems(photos, getRandomPositiveInteger(0, features.length)),
-    },
-    location: {
-      lat: getRandomLat,
-      lng: getRandomLng,
-    },
-  };
-};
+const createOffer = () => ({
+  avatar: `img/avatars/user0${getRandomPositiveInteger(mocksConfig.AVATAR.MIN, mocksConfig.AVATAR.MAX)}.png`,
+  offer: {
+    title: 'Добро пожаловать',
+    address: `${getRandomLat}, ${getRandomLng}`,
+    price: getRandomPositiveInteger(mocksConfig.PRICE.MIN, mocksConfig.PRICE.MAX),
+    type: getRandomItems(types, getRandomPositiveInteger(1, 1)),
+    rooms: getRandomPositiveInteger(mocksConfig.ROOMS.MIN, mocksConfig.ROOMS.MAX),
+    guests: getRandomPositiveInteger(mocksConfig.QUESTS.MIN, mocksConfig.QUESTS.MAX),
+    checkin: getRandomItems(checkins, getRandomPositiveInteger(1, 1)),
+    checkout: getRandomItems(checkouts, getRandomPositiveInteger(1, 1)),
+    features: getRandomItems(features, getRandomPositiveInteger(0, features.length)),
+    description: 'Комфортабельное и уютное помещение',
+    photos: getRandomItems(photos, getRandomPositiveInteger(0, features.length)),
+  },
+  location: {
+    lat: getRandomLat,
+    lng: getRandomLng,
+  },
+});
 
 // Создает новые рандомные объекты
-const createAuthors = (count) => new Array(count).fill(null).map(() => createAuthor());
-
-createAuthors(14);
+const createOffers = (count) => new Array(count).fill(null).map(() => createOffer());
+createOffers(14);
